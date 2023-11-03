1dd3b44845d6d9606d7b96f4625b093ae943dc47
matematiflo <7537413+matematiflo@users.noreply.github.com>
2023-11-03 19:09:05 +0900

    celanup package name

--- FormallyRealFields.lean
+++ FormallyRealFields.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `FormallyRealFields` library.
 -- Import modules here that should be built as part of the library.
-import «FormallyRealFields».SumSq.Defs
-import «FormallyRealFields».SumSq.Ppties
+import FormallyRealFields.SumSq.Defs
+import FormallyRealFields.SumSq.Ppties
